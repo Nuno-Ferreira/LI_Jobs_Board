39c975b6e1fd6eee2d24ffada235d324399d349e
Nuno Ferreira <nuno.ferreira@corepower.energy>
2024-12-12 22:08:47 +0000

    Updated urls

--- backend/linkedin_jobs/urls.py
+++ backend/linkedin_jobs/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .api import LinkedinJobListCreateAPI
+from .api import LinkedinJobListCreateAPI, LinkedinAuthURL, LinkedinCallback
 
 urlpatterns = [
-    path('jobs/', LinkedinJobListCreateAPI.as_view(), name='jobs')
+    path('jobs/', LinkedinJobListCreateAPI.as_view(), name='jobs'),
+    path('auth/linkedin/', LinkedinAuthURL.as_view(), name='linkedin_auth_url'),
+    path('auth/linkedin/callback/', LinkedinCallback.as_view(), name='linkedin_callback'),
 ]
